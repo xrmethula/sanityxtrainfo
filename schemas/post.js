976f037b5b6845f0e737b91ced477b18be360ee6
xrmethula <x.r.methula@gmail.com>
2022-01-18 17:38:46 +0200

    first commit

---
+++ schemas/post.js
@@ -0,0 +1,62 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'id',
+      title: 'ID',
+      type: 'number',
+    },
+    {
+      name: 'type',
+      title: 'Type',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Bursary', value: 'Bursary' },
+          { title: 'Internship', value: 'Internship' },
+          { title: 'Learnership', value: 'Learnership' },
+        ],
+        layout: 'dropDown',
+      },
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'overview',
+      title: 'Overview',
+      type: 'string',
+    },
+    // {
+    //   name: 'body',
+    //   title: 'Body',
+    //   type: 'blockContent',
+    // },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+  ],
+};
